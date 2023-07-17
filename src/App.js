870ea6fcd1c34d1e52a9dbd1f779c87ba9271000
ncp204 <19130172@st.hcmuaf.edu.vn>
2023-07-17 20:43:15 +0700

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 function App() {
   let [name, setName] = useState('NCP')
   const [address, setAddress] = useState('')
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Watching TV' },
+    { id: 'todo2', title: 'Listing music' }
+  ])
 
   const handleEventClick = () => {
-    setName(address)
+    //hook not merge stage
+    if (!address) {
+      toast.error('Error: Input is empty')
+      return;
+    }
+    let newTodo = { id: 'xxx', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('');
   }
 
   const handleOnChangeInput = (event) => {
@@ -21,9 +33,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello {name}</h1>
+        <div className='todo-container'>
+          {todos.map(todo => {
+            return (
+              <li className='todo-child' key={todo.id}>{todo.title}</li>
+            )
+          })}
+        </div>
         <input type='text' value={address} onChange={(event) => handleOnChangeInput(event)} />
         <button type='button' onClick={() => handleEventClick()}>Click me</button>
       </header>
+
     </div>
   );
 }
